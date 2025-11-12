e0a4e9c6f709dd127a73e494fb0e86f07cf7f5f4
Hoang Tran <narfmcfarf@gmail.com>
2025-11-12 10:57:38 -0800

    order screen promting and act

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,4 +1,7 @@
 package com.pluralsight;
 
 public class Order {
+    public void Order(){;
+    };
+
 }
--- src/main/java/com/pluralsight/Orderscreen.java
+++ src/main/java/com/pluralsight/Orderscreen.java
@@ -1,7 +1,60 @@
 package com.pluralsight;
 
-public class Orderscreen {
-    public void OpenOrderScreen (){
+import java.util.Scanner;
+
+public class Orderscreen extends Homescreen {
+
+
+    public static void OpenOrderScreen(Scanner scan) {
         System.out.println("AHHHHHHH");
+        System.out.println("Please add a name for the order");
+
+        System.out.println("""
+                Prompting order menu would you like to 
+                
+                1Add IceCream to your order
+                2Add a drink
+                3Add a snack 
+                4 *Other
+                5 Review your order 
+                
+                """);
+
+        String choice = scan.nextLine().toUpperCase().trim();
+        switch (choice) {
+            case "1":
+                System.out.println();
+
+                break;
+            case "2":
+                System.out.println("action 2");
+                break;
+            case "3":
+                System.out.println("action 3");
+
+                break;
+            case "4":
+                System.out.println("action 4");
+
+                break;
+            case "5":
+                System.out.println("action 5");
+
+                break;
+            case "x":
+                System.out.println("returning to Homescreen");
+
+                break;
+            case "M":
+                System.out.println("View menu");
+
+                break;
+            default:
+                System.out.println("invalid response");
+                break;
+
+
+        }
+
     }
 }
