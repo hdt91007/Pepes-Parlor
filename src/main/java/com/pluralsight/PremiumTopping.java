5c5a9643547cc9f8f8f80a5fb52974924e5a4987
Hoang Tran <narfmcfarf@gmail.com>
2025-11-12 10:59:07 -0800

    premium constructor/getter/setter and getprice based off size

--- src/main/java/com/pluralsight/PremiumTopping.java
+++ src/main/java/com/pluralsight/PremiumTopping.java
@@ -1,4 +1,71 @@
 package com.pluralsight;
 
 public class PremiumTopping {
+String name;
+double price;
+boolean extra;
+
+    public PremiumTopping(String name, double price, boolean extra) {
+        this.name = name;
+        this.price = price;
+        this.extra = extra;
+    }
+    public PremiumTopping(){};
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice(String Size) {
+
+        switch (Size) {
+            case "S":
+                if (extra){
+                    return 1.5;
+                }else {
+                    return 1.0;
+                }
+                //todo dynamic price by .5 value
+            case "M":
+                if (extra){
+                    return 3;
+                }else {
+                    return 2;
+                }
+            case "L":
+                if (extra){
+                    return 4.50;
+                }else {
+                    return 3.0;
+                }
+            default:
+                return 0;
+//todo add an invalid case for redo
+        }
+
+        //return price;
+        //todo auto adjust Size
+
+
+
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isExtra() {
+        return extra;
+    }
+
+    public void setExtra(boolean extra) {
+        this.extra = extra;
+    }
+
+
+    //Gold flake, Caviar, Cheesecake,
 }
--- src/main/java/com/pluralsight/Regulartopping.java
+++ src/main/java/com/pluralsight/Regulartopping.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
 public class Regulartopping {
+    String name;
+    String price;
+    boolean extra;
+
+    public Regulartopping(String name, String price, boolean extra) {
+        this.name = name;
+        this.price = price;
+        this.extra = extra;
+    }
 }
